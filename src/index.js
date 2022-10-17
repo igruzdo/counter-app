607d131903cb27ee57e652b7f389a655ad174fe6
Igor Gruzdo <igruzdo@yandex.ru>
2022-10-17 20:11:51 +0300

    done 193

--- src/index.js
+++ src/index.js
@@ -1,35 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { legacy_createStore, bindActionCreators } from 'redux';
+import { legacy_createStore } from 'redux';
 import './index.css';
 import { reducer } from './reducer';
-import * as actions from './action';
+import App from './components/App';
+import { Provider } from 'react-redux';
 
 const store = legacy_createStore(reducer)
 
-const { dispatch, subscribe, getState } = store
-
-// const bindActionCreator = (creator, dispatch) => (...args) => {
-//   dispatch(creator(...args))
-// }
-
-const { inc, dec, rnd } = bindActionCreators(actions, dispatch)
-
-document.querySelector('#inc').addEventListener('click', inc);
-document.querySelector('#dec').addEventListener('click', dec);
-document.querySelector('#rnd').addEventListener('click', () => {
-  const value = Math.floor(Math.random() * 10);
-  rnd(value);
-});
-
-const update = () => {
-  document.querySelector('#counter').textContent = getState().value
-}
-
-subscribe(update)
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-  </React.StrictMode>
-);
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App/>
+      </Provider>
+    </React.StrictMode>
+  );
+  
